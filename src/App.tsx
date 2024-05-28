de744c8caa0bae002868bdc61b7832be52de5fdc
elBanna00 <moaazibrahim3@gmail.com>
2024-05-29 00:59:33 +0300

    style:add visualizer boilerplate with initial styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import SortingVisualizer from "./components/SortingVisualizer";
 
 function App() {
-  return <div>Hello React!</div>;
+  return (
+    <div className="App">
+      <SortingVisualizer></SortingVisualizer>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/SortingVisualizer.tsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import "./SortingVisualizer.css";
+
+// Change this value for the speed of the animations.
+const ANIMATION_SPEED_MS = 1;
+
+// Change this value for the number of bars (value) in the array.
+const NUMBER_OF_ARRAY_BARS = 250;
+
+// This is the main color of the array bars.
+const PRIMARY_COLOR = "pink";
+
+// This is the color of array bars that are being compared throughout the animations.
+const SECONDARY_COLOR = "green";
+
+const SortingVisualizer: React.FC = () => {
+  const [array, setArray] = useState<number[]>([]);
+
+  useEffect(() => {
+    resetArray();
+  }, []);
+
+  const resetArray = () => {
+    const newArray = [];
+    for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+      newArray.push(randomIntFromInterval(5, 730));
+    }
+    setArray(newArray);
+  };
+
+  return (
+    <>
+      <header>Sorting Visualizer</header>
+      <div className="array-container">
+        {array.map((value, idx) => (
+          <div
+            className="array-bar"
+            key={idx}
+            style={{
+              backgroundColor: PRIMARY_COLOR,
+              height: `${value}px`,
+            }}
+          ></div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+// From https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+const randomIntFromInterval = (min: number, max: number): number => {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const arraysAreEqual = (arrayOne: number[], arrayTwo: number[]): boolean => {
+  if (arrayOne.length !== arrayTwo.length) return false;
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export default SortingVisualizer;
